962e420697b5eacecc46b0895a1946b9c6c3cb48
Kriti Majumdar <86869858+Kriti2019@users.noreply.github.com>
2024-09-03 11:58:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    // Your code goes here
+	    int m = str1.size();
+    int n = str2.size();
+
+    // Create a DP table to store lengths of longest common subsequence
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Fill the DP table
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+        // Length of LCS
+    int lcs_length = dp[m][n];
+
+    // Calculate the number of deletions and insertions
+    int deletions = m - lcs_length;
+    int insertions = n - lcs_length;
+
+    return deletions + insertions;
+
+	    
+	} 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
