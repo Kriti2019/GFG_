e41ca29a86ce3f9c7e5cfb4371526487be80a0da
Kriti Majumdar <86869858+Kriti2019@users.noreply.github.com>
2024-09-15 22:57:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -6,26 +6,35 @@ using namespace std;
 class Solution{
 
 	public:
+int mod =(int)1e9+7;
+int countPartitionsUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+
+     if(ind == 0){
+        if(target==0 && arr[0]==0)
+            return 2;
+        if(target==0 || target == arr[0])
+            return 1;
+        return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= (notTaken + taken)%mod;
+}
+
 	int perfectSum(int arr[], int n, int sum)
 	{
-        const int MOD = 1e9 + 7;
-
-        // Initialize a vector 'dp' where dp[j] stores the number of ways to make sum 'j'
-        vector<int> dp(sum + 1, 0);
-        
-        // Base case: There's 1 way to make sum 0 (by selecting no elements)
-        dp[0] = 1;
-        
-        // Traverse each element of the array
-        for (int i = 0; i < n; i++) {
-            // Update the DP array in reverse to avoid overwriting previous results
-            for (int j = sum; j >= arr[i]; j--) {
-                dp[j] = (dp[j] + dp[j - arr[i]]) % MOD;
-            }
-        }
-
-        // The final result is the number of ways to form the sum 'sum'
-        return dp[sum];
+	    vector<int> vec(arr, arr + n);  // Convert array to vector
+	    vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+	    
+        return countPartitionsUtil(n-1,sum,vec,dp);
 	}
 	  
 };
