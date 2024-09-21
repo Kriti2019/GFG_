a2c10e597417e65240072f6e84994e082b9d6b26
Kriti Majumdar <86869858+Kriti2019@users.noreply.github.com>
2024-09-21 21:28:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.cpp
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  void subsetSumsHelper(int ind, vector < int > & arr, int n, vector < int > & ans, int sum) {
+   void subsetSumsHelper(int ind, vector < int > & arr, int n, vector < int > & ans, int sum) {
       if (ind == n) {
         ans.push_back(sum);
         return;
@@ -16,12 +16,13 @@ class Solution {
       //element is not picked
       subsetSumsHelper(ind + 1, arr, n, ans, sum);
     }
+   
     vector<int> subsetSums(vector<int> arr, int n) {
-    // Write Your Code here
-    vector < int > ans;
-    subsetSumsHelper(0, arr, n, ans, 0);
-    sort(ans.begin(), ans.end());
-    return ans;
+        // Write Your Code here
+        vector < int > ans;
+        subsetSumsHelper(0, arr, n, ans, 0);
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
 
