0e7da5b741b52221049f77bb5d85b0511792059b
Kriti Majumdar <86869858+Kriti2019@users.noreply.github.com>
2024-09-29 10:42:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Total count/total-count.py
@@ -0,0 +1,34 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def totalCount(self, k, arr):
+        total_count = 0
+        
+        for num in arr:
+            # Calculate the number of parts needed for each number
+            total_count += (num + k - 1) // k  # This is equivalent to math.ceil(num / k)
+        
+        return total_count
+
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        k= int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.totalCount(k,arr)
+        print(res)
+        t -= 1
+
+
+# } Driver Code Ends
