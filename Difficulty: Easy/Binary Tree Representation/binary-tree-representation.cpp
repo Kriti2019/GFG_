70473369bd7b482595b37a5a5c5b57d102bae6c0
Kriti Majumdar <86869858+Kriti2019@users.noreply.github.com>
2024-10-14 21:43:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Tree Representation/binary-tree-representation.cpp
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}
+
+void traverseInOrder(struct node *temp, vector<int> &inorder) {
+
+  if (temp != NULL) {
+    traverseInOrder(temp->left,inorder);
+    // cout << " " << temp->data;
+    inorder.push_back(temp->data);
+    traverseInOrder(temp->right,inorder);
+  }
+  return;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}*/
+
+class Solution{
+public:
+
+    void create_tree(node* root0, std::vector<int>& vec) {
+        // Edge case: if the array is empty, we cannot create the tree.
+        if (vec.empty()) return;
+
+        // Initialize a queue to perform the level order construction.
+        std::queue<node*> q;
+        q.push(root0);
+
+        int i = 1; // Start from index 1 as vec[0] is the root.
+        while (!q.empty() && i < vec.size()) {
+            // Get the current node from the queue.
+            node* current = q.front();
+            q.pop();
+
+            // Assign the left child if possible.
+            if (i < vec.size()) {
+                current->left = newNode(vec[i]);
+                q.push(current->left);
+                i++;
+            }
+
+            // Assign the right child if possible.
+            if (i < vec.size()) {
+                current->right = newNode(vec[i]);
+                q.push(current->right);
+                i++;
+            }
+        }
+    }
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+        vector<int> vec(7);
+        for(int i = 0;i<7;i++){
+            cin >> vec[i];
+        }
+        struct node *root0 = newNode(vec[0]);
+        root0->left = newNode(vec[1]);
+        root0->right = newNode(vec[2]);
+        root0->left->left = newNode(vec[3]);
+        root0->left->right = newNode(vec[4]);
+        root0->right->left = newNode(vec[5]);
+        root0->right->right = newNode(vec[6]);
+
+        Solution obj;
+        struct node *root = newNode(vec[0]);
+        obj.create_tree(root, vec);
+
+        vector<int> a;
+        traverseInOrder(root0,a);
+        vector<int> b;
+        traverseInOrder(root,b);
+        if(a == b) cout << 1 << endl;
+        else cout << -1 << endl;
+
+       
+        
+
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
