d94b41776c6e4daf164fdfb6cfcc3bff17c9ed5b
Kriti Majumdar <86869858+Kriti2019@users.noreply.github.com>
2024-11-19 12:50:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Permutation/next-permutation.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& nums) {
+        // code here
+        int n = nums.size(); // size of the array.
+
+        // Step 1: Find the break point:
+        int ind = -1; // break point
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                // index i is the break point
+                ind = i;
+                break;
+            }
+        }
+
+        // If break point does not exist:
+        if (ind == -1) {
+            // reverse the whole array:
+            reverse(nums.begin(), nums.end());
+            return; // Exit the function after reversing
+        }
+
+        // Step 2: Find the next greater element
+        //         and swap it with arr[ind]:
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+
+        // Step 3: reverse the right half:
+        if (ind >= 0 && ind < nums.size()) {
+            std::reverse(nums.begin() + ind + 1, nums.end());
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.nextPermutation(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
